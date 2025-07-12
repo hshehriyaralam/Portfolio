5efa9eada3c8a083c5f625686927fbd0c7106a8b
hshehriyaralam <hshehriyaralam@gmail.com>
2025-07-12 19:14:05 +0500

    fetch Description in work,hero about section in Portfolio

--- src/app/Admin/page.tsx
+++ src/app/Admin/page.tsx
@@ -2,11 +2,13 @@
 import styles from '../../Styles/styles.module.css'
 import { ContextTheme } from '../../Context/ThemeContext'
 import { ContextDescription } from '../../Context/DescriptionContext'
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
+import { Pencil } from 'lucide-react'
 
 export default function Admin() {
   const { themeValue } = useContext(ContextTheme)
   const {  loading, getDescriptionBySection } = useContext(ContextDescription)
+  const [isEditing, SetIsEditing] = useState(false)
 
   // Loading state
   if (loading) {
@@ -24,29 +26,52 @@ export default function Admin() {
       >
         Admin
       </h1>
+      
       <div
         className={`grid grid-cols-1 md:grid-cols-2 gap-8`}
       >
         {/* Hero Section */}
+        
         <div
           className={`border rounded-xl p-6 shadow-md hover:-translate-y-1 duration-500 ${
             themeValue ? 'border-gray-300 bg-white' : 'border-gray-700 bg-gray-800'
           }`}
         >
-          <h2
+         
+
+         <div  className='flex items-center justify-between'>
+            <h2
             className={`text-xl font-semibold mb-2 ${
               themeValue ? 'text-gray-700' : 'text-gray-200'
             }`}
-          >
+            >
             Hero Section Description
           </h2>
-          <p
-            className={`text-sm ${
+          {!isEditing &&
+           <Pencil 
+          onClick={() => SetIsEditing(true)}
+          size={18} className={`${themeValue ? 'text-black' : 'text-white'} cursor-pointer  `} />
+          }
+         
+            </div>
+          {isEditing ? (
+            <input
+              className={`text-md px-3 py-3 rounded-md border outline-none w-full max-w-md ${
+                themeValue ? 'bg-white text-gray-800 border-gray-300' : 'bg-transparent text-gray-200 border-gray-600'
+              }`}
+              autoFocus
+            />
+          ) : (
+            <div className="flex items-center gap-2">
+              <p
+              className={`text-sm ${
               themeValue ? 'text-gray-800' : 'text-gray-300'
             }`}
-          >
-            {getDescriptionBySection('hero')}
-          </p>
+              >
+                {getDescriptionBySection('hero')}
+              </p>
+            </div>
+          )}
         </div>
         {/* About Section */}
         <div
@@ -88,7 +113,7 @@ export default function Admin() {
               themeValue ? 'text-gray-800' : 'text-gray-300'
             }`}
           >
-            {getDescriptionBySection('workShort')}
+            {getDescriptionBySection('workShot')}
           </p>
         </div>
 
